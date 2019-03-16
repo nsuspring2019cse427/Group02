7cb60bfa370f40f22e3cdbd26d7ae678ecc136c1
mantasarul4982 <mantasarul.elahi@northsouth.edu>
2019-03-17 03:28:11 +0600

    Added Email validator,Shared preference managerand other necessary code

---
+++ Project01/Code/Project01/app/src/main/java/com/cse427/group02/project01/SharedPreferenceEntry.java
@@ -0,0 +1,33 @@
+package com.cse427.group02.project01;
+
+/**
+ * Created by Syam on 24-Feb-19.
+ */
+
+import java.util.Calendar;
+
+/**
+ * Model class containing personal information that will be saved to SharedPreferences.
+ */
+public class SharedPreferenceEntry {
+    // Name of the user.
+    private final String mName;
+    // Date of Birth of the user.
+    private final Calendar mDateOfBirth;
+    // Email address of the user.
+    private final String mEmail;
+    public SharedPreferenceEntry(String name, Calendar dateOfBirth, String email) {
+        mName = name;
+        mDateOfBirth = dateOfBirth;
+        mEmail = email;
+    }
+    public String getName() {
+        return mName;
+    }
+    public Calendar getDateOfBirth() {
+        return mDateOfBirth;
+    }
+    public String getEmail() {
+        return mEmail;
+    }
+}
